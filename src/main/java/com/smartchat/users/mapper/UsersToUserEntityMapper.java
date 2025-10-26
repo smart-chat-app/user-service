1a8ad65968e8fe6b8668c3f00f278e49f97c2def
frankye961 <strikeaglef15d@gmail.com>
2025-10-26 20:29:53 +0100

    fixing NPE

--- src/main/java/com/smartchat/users/mapper/UsersToUserEntityMapper.java
+++ src/main/java/com/smartchat/users/mapper/UsersToUserEntityMapper.java
@@ -4,14 +4,15 @@ import com.smartchat.users.model.User;
 import com.smartchat.users.persistance.model.UserEntity;
 
 import java.time.Instant;
+import java.util.Objects;
 import java.util.UUID;
 
 public class UsersToUserEntityMapper {
     public static UserEntity map(User user){
         return UserEntity.builder()
                 .userId(UUID.randomUUID().toString())
-                .bio(user.getBio().get())
-                .avatarUrl(user.getAvatarUrl().get().toString())
+                .bio(Objects.isNull(user.getBio().get()) ? "" : user.getBio().get())
+                .avatarUrl(Objects.isNull(user.getAvatarUrl().get().toString()) ? "" : user.getAvatarUrl().get().toString())
                 .username(user.getUsername())
                 .displayName(user.getDisplayName())
                 .createdAt(Instant.now())
